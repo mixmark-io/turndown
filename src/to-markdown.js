b51f3450b7d7f2720acda9cc2f8a3cd0af3bcf51
domchristie <dom@thinkpigment.com>
2011-10-23 11:34:26 +0100

    Initial commit

---
+++ src/to-markdown.js
@@ -0,0 +1,132 @@
+// √ paragraphs
+// √ linebreaks - voidEl
+// √ headers
+// blockquotes
+// √ lists
+// code blocks
+// √ horizontal rules - voidEl
+// 
+// √ links
+// √ emphasis (em, strong)
+// √ code
+// √ images - voidEl
+
+// inline html
+
+var toMarkdown = function(string) {
+  
+  var ELEMENTS = [
+    {
+      patterns: 'p',
+      replacement: function(str, attrs, innerHTML) {
+        return innerHTML ? '\n\n' + innerHTML + '\n\n' : '';
+      }
+    },
+    {
+      patterns: 'br',
+      type: 'void',
+      replacement: '\n'
+    },
+    {
+      patterns: 'h([1-6])',
+      replacement: function(str, hLevel, attrs, innerHTML) {
+        var hPrefix = '';
+        for(var i = 0; i < hLevel; i++) {
+          hPrefix += '#';
+        }
+        return '\n\n' + hPrefix + ' ' + innerHTML + '\n\n';
+      }
+    },
+    {
+      patterns: 'hr',
+      type: 'void',
+      replacement: '\n\n* * *\n\n'
+    },
+    {
+      patterns: 'a',
+      replacement: function(str, attrs, innerHTML) {
+        var href = attrs.match(attrRegExp('href')),
+            title = attrs.match(attrRegExp('title'));
+        return '[' + innerHTML + ']' + '(' + href[1] + (title && title[1] ? ' "' + title[1] + '"' : '') + ')';
+      }
+    },
+    {
+      patterns: ['b', 'strong'],
+      replacement: function(str, attrs, innerHTML) {
+        return innerHTML ? '**' + innerHTML + '**' : '';
+      }
+    },
+    {
+      patterns: ['i', 'em'],
+      replacement: function(str, attrs, innerHTML) {
+        return innerHTML ? '_' + innerHTML + '_' : '';
+      }
+    },
+    {
+      patterns: 'code',
+      replacement: function(str, attrs, innerHTML) {
+        return innerHTML ? '`' + innerHTML + '`' : '';
+      }
+    },
+    {
+      patterns: 'img',
+      type: 'void',
+      replacement: function(str, attrs, innerHTML) {
+        var src = attrs.match(attrRegExp('src')),
+            alt = attrs.match(attrRegExp('alt')),
+            title = attrs.match(attrRegExp('title'));
+        return '![' + (alt && alt[1] ? alt[1] : '') + ']' + '(' + src[1] + (title && title[1] ? ' "' + title[1] + '"' : '') + ')';
+      }
+    }
+  ];
+  
+  for(var i = 0, len = ELEMENTS.length; i < len; i++) {
+    if(typeof ELEMENTS[i].patterns === 'string') {
+      string = replaceEls(string, { tag: ELEMENTS[i].patterns, replacement: ELEMENTS[i].replacement, type:  ELEMENTS[i].type });
+    }
+    else {
+      for(var j = 0, pLen = ELEMENTS[i].patterns.length; j < pLen; j++) {
+        string = replaceEls(string, { tag: ELEMENTS[i].patterns[j], replacement: ELEMENTS[i].replacement, type:  ELEMENTS[i].type });
+      }
+    }
+  }
+  
+  function replaceEls(html, elProperties) {
+    var pattern = elProperties.type === 'void' ? '<' + elProperties.tag + '([^>]*)\\/?>' : '<' + elProperties.tag + '([^>]*)>(.*)<\\/' + elProperties.tag + '>',
+        regex = new RegExp(pattern, 'gi'),
+        markdown = '';
+    if(typeof elProperties.replacement === 'string') {
+      markdown = html.replace(regex, elProperties.replacement);
+    }
+    else {
+      markdown = html.replace(regex, function(str, p1, p2, p3) {
+        return elProperties.replacement.call(this, str, p1, p2, p3);
+      });
+    }
+    return markdown;
+  }
+  
+  function attrRegExp(attr) {
+    return new RegExp(attr + '\\s*=\\s*["\']?([^"\']*)["\']?', 'i');
+  }
+  
+  // Todo:
+  // Code blocks
+  // Blockquotes
+  
+  // Lists
+  string = string.replace(/<(ul|ol)[^>]*>(.*)<\/(?:ul|ol)>/gi, function(str, listType, innerHTML) {
+    var lis = innerHTML.split('</li>');
+    for(i = 0, len = lis.length; i < len; i++) {
+      if(lis[i]) {
+        var prefix = (listType === 'ol') ? (i + 1) + ". " : "* ";
+        lis[i] = lis[i].replace(/<li[^>]*>(.*)/i, function(str, innerHTML) {
+          return prefix + innerHTML + '\n';
+        });
+      }
+    }
+    return "\n\n" + lis.join('') + "\n";
+  });
+  
+  return string;
+};
---
+++ test/tests.js
@@ -0,0 +1,66 @@
+$(function(){
+  
+  test("converting p elements", function() {
+    equal(toMarkdown("<p>Lorem ipsum</p>"), "\n\nLorem ipsum\n\n", "We expect p tags to be wrapped with two line breaks");
+    equal(toMarkdown("<p class='intro'>Lorem ipsum</p>"), "\n\nLorem ipsum\n\n", "We expect p tags to be wrapped with two line breaks");
+  });
+  
+  test("converting emphasis elements", function() {
+    equal(toMarkdown("<b>Hello world</b>"), "**Hello world**", "We expect <b>Hello world</b> to be converted to **Hello world**");
+    equal(toMarkdown("<strong>Hello world</strong>"), "**Hello world**", "We expect <strong>Hello world</strong> to be converted to **Hello world**");
+    equal(toMarkdown("<b></b>"), "", "We expect b tags to be removed");
+    
+    equal(toMarkdown("<i>Hello world</i>"), "_Hello world_", "We expect <i>Hello world</i> to be converted to _Hello world_");
+    equal(toMarkdown("<em>Hello world</em>"), "_Hello world_", "We expect <em>Hello world</em> to be converted to _Hello world_");
+    equal(toMarkdown("<em id='one' class='cowabunga'>Hello world</em>"), "_Hello world_", "We expect <em id='one' class='cowabunga'>Hello world</em> to be converted to _Hello world_");
+    equal(toMarkdown("<em id='one' class='cowabunga'></em>"), "", "We expect empty em tags to be removed");
+  });
+  
+  test("converting inline code elements", function() {
+    equal(toMarkdown("<code>print()</code>"), "`print()`", "We expect inline code tags to be converted to backticks");
+    equal(toMarkdown("<code></code>"), "", "We expect empty code tags to be removed");
+  });
+  
+  test("converting heading elements", function() {
+    equal(toMarkdown("<h1>Hello world</h1>"), "\n\n# Hello world\n\n", "We expect <h1>Hello world</h1> to be converted to # Hello world");
+    equal(toMarkdown("<h3>Hello world</h3>"), "\n\n### Hello world\n\n", "We expect <h3>Hello world</h3> to be converted to ### Hello world");
+    equal(toMarkdown("<h6>Hello world</h6>"), "\n\n###### Hello world\n\n", "We expect <h6>Hello world</h6> to be converted to ###### Hello world");
+    
+    equal(toMarkdown("<h8>Hello world</h8>"), "<h8>Hello world</h8>", "We expect <h8>Hello world</h8> to be converted to <h8>Hello world</h8>");
+  });
+  
+  test("converting hr elements", function() {
+    equal(toMarkdown("<hr />"), "\n\n* * *\n\n", "We expect hr elements to be converted to * * *");
+    equal(toMarkdown("<hr/>"), "\n\n* * *\n\n", "We expect hr elements to be converted to * * *");
+    equal(toMarkdown("<hr>"), "\n\n* * *\n\n", "We expect hr elements to be converted to * * *");
+    equal(toMarkdown("<hr class='fancy' />"), "\n\n* * *\n\n", "We expect hr elements to be converted to * * *");
+  });
+  
+  test("converting br elements", function() {
+    equal(toMarkdown("<br />"), "\n", "We expect br elements to be converted to \n");
+    equal(toMarkdown("<br/>"), "\n", "We expect br elements to be converted to \n");
+    equal(toMarkdown("<br>"), "\n", "We expect br elements to be converted to \n");
+  });
+  
+  test("converting img elements", function() {
+    equal(toMarkdown("<img src='http://example.com/logo.png' />"), "![](http://example.com/logo.png)", "We expect img elements to be converted properly");
+    equal(toMarkdown('<img src="http://example.com/logo.png" />'), "![](http://example.com/logo.png)", "We expect img elements to be converted properly");
+    equal(toMarkdown("<img src='http://example.com/logo.png'>"), "![](http://example.com/logo.png)", "We expect img elements to be converted properly");
+    equal(toMarkdown("<img src=http://example.com/logo.png>"), "![](http://example.com/logo.png)", "We expect img elements to be converted properly");
+
+    equal(toMarkdown("<img src='http://example.com/logo.png' alt='Example logo' />"), "![Example logo](http://example.com/logo.png)", "We expect img elements to be converted properly with alt attrs");
+    equal(toMarkdown("<img src='http://example.com/logo.png' alt='Example logo' title='Example title' />"), "![Example logo](http://example.com/logo.png \"Example title\")", "We expect img elements to be converted properly with alt and title attrs");
+  });
+  
+  test("converting anchor elements", function() {
+    equal(toMarkdown("<a href='http://example.com/about'>About us</a>"), "[About us](http://example.com/about)", "We expect anchor elements to be converted properly");
+    equal(toMarkdown('<a href="http://www.example.com/about" title="About this company">About us</a>'), '[About us](http://www.example.com/about "About this company")', "We expect an anchor element with a title tag to have correct markdown");
+    equal(toMarkdown('<a class="some really messy stuff" href="/about" id="donuts3" title="About this company">About us</a>'), '[About us](/about "About this company")', "We expect an anchor element with a title tag to have correct markdown");
+  });
+  
+  test("converting list elements", function() {
+    equal(toMarkdown("<ol><li>Hello world</li><li>Lorem ipsum</li></ol>"), "\n\n1. Hello world\n2. Lorem ipsum\n\n", "We expect ol elements to be converted properly");
+    equal(toMarkdown("<ul><li>Hello world</li><li>Lorem ipsum</li></ul>"), "\n\n* Hello world\n* Lorem ipsum\n\n", "We expect ol elements to be converted properly");
+    equal(toMarkdown("<ul class='blargh'><li class='first'>Hello world</li><li>Lorem ipsum</li></ul>"), "\n\n* Hello world\n* Lorem ipsum\n\n", "We expect ol elements to be converted properly");
+  });
+});
