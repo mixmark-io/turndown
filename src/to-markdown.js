225fa50f59ab6da9ae2f7924064f1bc27b819c5b
domchristie <dom@thinkpigment.com>
2011-10-23 22:19:50 +0100

    Remove debug code

--- src/to-markdown.js
+++ src/to-markdown.js
@@ -106,7 +106,6 @@ var toMarkdown = function(string) {
   // Lists
   // use [\s\S] to match any char (rather than .) to fix . not matching across linebreaks: http://www.regular-expressions.info/dot.html#nodotall
   string = string.replace(/<(ul|ol)\b[^>]*>([\s\S]*?)<\/\1>/gi, function(str, listType, innerHTML) {
-    window.console && console.log(str);
     var lis = innerHTML.split('</li>');
     for(i = 0, len = lis.length; i < len; i++) {
       if(lis[i]) {
