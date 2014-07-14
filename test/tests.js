c7253cc56d9e44efdbbf37d812113f4aaff8346f
Dom Christie <christiedom@gmail.com>
2014-07-14 20:37:38 +0100

    Remove traling whitespace from tests

--- test/tests.js
+++ test/tests.js
@@ -1,47 +1,47 @@
 $(function(){
-  
+
   test("converting p elements", function() {
     equal(toMarkdown("<p>Lorem ipsum</p>"), "Lorem ipsum", "We expect p tags to be wrapped with two line breaks");
     equal(toMarkdown("<p class='intro'>Lorem ipsum</p>"), "Lorem ipsum", "We expect p tags to be wrapped with two line breaks");
   });
-  
+
   test("converting emphasis elements", function() {
     equal(toMarkdown("<b>Hello world</b>"), "**Hello world**", "We expect <b>Hello world</b> to be converted to **Hello world**");
     equal(toMarkdown("<strong>Hello world</strong>"), "**Hello world**", "We expect <strong>Hello world</strong> to be converted to **Hello world**");
     equal(toMarkdown("<b></b>"), "", "We expect b tags to be removed");
-    
+
     equal(toMarkdown("<i>Hello world</i>"), "_Hello world_", "We expect <i>Hello world</i> to be converted to _Hello world_");
     equal(toMarkdown("<em>Hello world</em>"), "_Hello world_", "We expect <em>Hello world</em> to be converted to _Hello world_");
     equal(toMarkdown("<em id='one' class='cowabunga'>Hello world</em>"), "_Hello world_", "We expect <em id='one' class='cowabunga'>Hello world</em> to be converted to _Hello world_");
     equal(toMarkdown("<em id='one' class='cowabunga'></em>"), "", "We expect empty em tags to be removed");
   });
-  
+
   test("converting inline code elements", function() {
     equal(toMarkdown("<code>print()</code>"), "`print()`", "We expect inline code tags to be converted to backticks");
     equal(toMarkdown("<code></code>"), "", "We expect empty code tags to be removed");
   });
-  
+
   test("converting heading elements", function() {
     equal(toMarkdown("<h1>Hello world</h1>"), "# Hello world", "We expect <h1>Hello world</h1> to be converted to # Hello world");
     equal(toMarkdown("<h3>Hello world</h3>"), "### Hello world", "We expect <h3>Hello world</h3> to be converted to ### Hello world");
     equal(toMarkdown("<h6>Hello world</h6>"), "###### Hello world", "We expect <h6>Hello world</h6> to be converted to ###### Hello world");
-    
+
     equal(toMarkdown("<h8>Hello world</h8>"), "<h8>Hello world</h8>", "We expect <h8>Hello world</h8> to be converted to <h8>Hello world</h8>");
   });
-  
+
   test("converting hr elements", function() {
     equal(toMarkdown("<hr />"), "* * *", "We expect hr elements to be converted to * * *");
     equal(toMarkdown("<hr/>"), "* * *", "We expect hr elements to be converted to * * *");
     equal(toMarkdown("<hr>"), "* * *", "We expect hr elements to be converted to * * *");
     equal(toMarkdown("<hr class='fancy' />"), "* * *", "We expect hr elements to be converted to * * *");
   });
-  
+
   test("converting br elements", function() {
     equal(toMarkdown("Hello<br />world"), "Hello\nworld", "We expect br elements to be converted to \n");
     equal(toMarkdown("Hello<br/>world"), "Hello\nworld", "We expect br elements to be converted to \n");
     equal(toMarkdown("Hello<br>world"), "Hello\nworld", "We expect br elements to be converted to \n");
   });
-  
+
   test("converting img elements", function() {
     equal(toMarkdown("<img src='http://example.com/logo.png' />"), "![](http://example.com/logo.png)", "We expect img elements to be converted properly");
     equal(toMarkdown('<img src="http://example.com/logo.png" />'), "![](http://example.com/logo.png)", "We expect img elements to be converted properly");
@@ -51,14 +51,14 @@ $(function(){
     equal(toMarkdown("<img src='http://example.com/logo.png' alt='Example logo' />"), "![Example logo](http://example.com/logo.png)", "We expect img elements to be converted properly with alt attrs");
     equal(toMarkdown("<img src='http://example.com/logo.png' alt='Example logo' title='Example title' />"), "![Example logo](http://example.com/logo.png \"Example title\")", "We expect img elements to be converted properly with alt and title attrs");
   });
-  
+
   test("converting anchor elements", function() {
     equal(toMarkdown("<a href='http://example.com/about'>About us</a>"), "[About us](http://example.com/about)", "We expect anchor elements to be converted properly");
     equal(toMarkdown('<a href="http://www.example.com/about" title="About this company">About us</a>'), '[About us](http://www.example.com/about "About this company")', "We expect an anchor element with a title tag to have correct markdown");
     equal(toMarkdown('<a class="some really messy stuff" href="/about" id="donuts3" title="About this company">About us</a>'), '[About us](/about "About this company")', "We expect an anchor element with a title tag to have correct markdown");
     equal(toMarkdown('<a id="donuts3">About us</a>'), '<a id="donuts3">About us</a>', "Anchor tags without an href should not be converted");
   });
-  
+
   test("converting code blocks", function() {
     var codeHtml = [
       "<pre><code>def hello_world",
@@ -72,7 +72,7 @@ $(function(){
     ];
     equal(toMarkdown(codeHtml.join('\n')), codeMd.join('\n'), "We expect code blocks to be converted");
   });
-  
+
   test("converting list elements", function() {
     equal(toMarkdown('1986. What a great season.'), '1986\\. What a great season.','We expect numbers that could trigger an ol to be escaped');
     equal(toMarkdown("<ol>\n\t<li>Hello world</li>\n\t<li>Lorem ipsum</li>\n</ol>"), "1.  Hello world\n2.  Lorem ipsum", "We expect ol elements to be converted properly");
@@ -80,7 +80,7 @@ $(function(){
     equal(toMarkdown("<ul class='blargh'><li class='first'>Hello world</li><li>Lorem ipsum</li></ul>"), "*   Hello world\n*   Lorem ipsum", "We expect ul elements with attributes to be converted properly");
     equal(toMarkdown("<ul><li>Hello world</li><li>Lorem ipsum</li></ul><ul><li>Hello world</li><li>Lorem ipsum</li></ul>"), "*   Hello world\n*   Lorem ipsum\n\n*   Hello world\n*   Lorem ipsum", "We expect multiple ul elements to be converted properly");
     equal(toMarkdown("<ul><li><p>Hello world</p></li><li>Lorem ipsum</li></ul>"), "*   Hello world\n\n*   Lorem ipsum", "We expect li elements with ps to be converted properly");
-    
+
     var lisWithPsHtml = [
       "<ol>",
       "  <li>",
@@ -92,7 +92,7 @@ $(function(){
       "  </li>",
       "</ol>"
     ].join('\n'),
-    
+
     lisWithPsMd = [
       "1.  This is a list item with two paragraphs. Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aliquam hendrerit mi posuere lectus.",
       "",
@@ -100,9 +100,9 @@ $(function(){
       "",
       "2.  Suspendisse id sem consectetuer libero luctus adipiscing."
     ].join('\n');
-    
+
     equal(toMarkdown(lisWithPsHtml), lisWithPsMd,'We expect lists with paragraphs to be converted');
-    
+
     var nestedListHtml = [
       "<ul>",
       "  <li>This is a list item at root level</li>",
@@ -134,7 +134,7 @@ $(function(){
       "*   This is a third item at root level"
     ].join('\n');
     equal(toMarkdown(nestedListHtml), nestedListMd, "We expect nested lists to be converted properly");
-    
+
     nestedListHtml = [
       "<ul>",
       "  <li>This is a list item at root level</li>",
@@ -166,7 +166,7 @@ $(function(){
       "*   This is a third item at root level"
     ].join('\n');
     equal(toMarkdown(nestedListHtml), nestedListMd, "We expect nested lists to be converted properly");
-    
+
     var html = [
       "<ul>",
       "  <li>",
@@ -182,11 +182,11 @@ $(function(){
       "",
       "    > This is a blockquote inside a list item."
     ].join('\n');
-    
+
     // needs fixing: see https://github.com/domchristie/to-markdown/issues/2
     equal(toMarkdown(html), md, "We expect lists with blockquotes to be converted");
   });
-  
+
   test("converting blockquotes", function() {
     var html = [
       "<blockquote>",
@@ -201,7 +201,7 @@ $(function(){
       "> Donec sit amet nisl. Aliquam semper ipsum sit amet velit. Suspendisse id sem consectetuer libero luctus adipiscing."
     ].join('\n');
     equal(toMarkdown(html), md, "We expect blockquotes with two paragraphs to be converted");
-    
+
     html = [
       "<blockquote>",
       "  <p>This is the first level of quoting.</p>",
@@ -221,7 +221,7 @@ $(function(){
       "> Back to the first level."
     ].join('\n');
     equal(toMarkdown(html), md, "We expect nested blockquotes to be converted");
-    
+
     html = [
       "<blockquote>",
       "  <h2>This is a header.</h2>",
