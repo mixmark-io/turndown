e5e8d6effd6ad873afe7ed32eed8587be6579e77
Dom Christie <christiedom@gmail.com>
2015-04-19 16:59:33 +0100

    Tidy gfm tests

--- test/gfm-test.js
+++ test/gfm-test.js
@@ -1,80 +1,99 @@
+/* global QUnit, test, equal */
+
+'use strict';
+
 if (typeof module !== 'undefined' && module.exports) {
   var toMarkdown = require('../index');
 }
 
 QUnit.module('GitHub Flavored Markdown');
 
+// Test cases are in the format: [html, expectedMarkdown, message];
+function runGfmTestCases(testCases) {
+  for (var i = 0; i < testCases.length; i++) {
+    var testCase = testCases[i];
+    equal(toMarkdown(testCase[0], { gfm: true }), testCase[1], testCase[2]);
+  }
+}
+
 test('strikethroughs', function() {
-  equal(toMarkdown('<del>Lorem ipsum</del>'), '~~Lorem ipsum~~', 'del');
-  equal(toMarkdown('<s>Lorem ipsum</s>'), '~~Lorem ipsum~~', 's');
-  equal(toMarkdown('<strike>Lorem ipsum</strike>'), '~~Lorem ipsum~~', 'strike');
+  runGfmTestCases([
+    ['<del>Lorem ipsum</del>', '~~Lorem ipsum~~', 'del'],
+    ['<s>Lorem ipsum</s>', '~~Lorem ipsum~~', 's'],
+    ['<strike>Lorem ipsum</strike>', '~~Lorem ipsum~~', 'strike']
+  ]);
 });
 
 test('task lists', function() {
-  equal(toMarkdown('<ul><li><input type=checkbox>Check Me!</li></ul>'), '*   [ ] Check Me!', 'Unchecked inputs');
-  equal(toMarkdown('<ul><li><input type=checkbox checked>Checked!</li></ul>'), '*   [x] Checked!', 'Checked inputs');
+  runGfmTestCases([
+    [
+      '<ul><li><input type=checkbox>Check Me!</li></ul>',
+      '*   [ ] Check Me!',
+      'Unchecked inputs'
+    ],
+    [
+      '<ul><li><input type=checkbox checked>Checked!</li></ul>',
+      '*   [x] Checked!',
+      'Checked inputs'
+    ]
+  ]);
 });
 
 test('tables', function() {
-  var html = [
-    '<table>',
-    '  <thead>',
-    '    <tr>',
-    '      <th>Column 1</th>',
-    '      <th>Column 2</th>',
-    '    </tr>',
-    '  </thead>',
-    '  <tbody>',
-    '    <tr>',
-    '      <td>Row 1, Column 1</td>',
-    '      <td>Row 1, Column 2</td>',
-    '    </tr>',
-    '    <tr>',
-    '      <td>Row 2, Column 1</td>',
-    '      <td>Row 2, Column 2</td>',
-    '    </tr>',
-    '  </tbody',
-    '</table>'
-  ].join('\n');
+  runGfmTestCases([
+    [
+      ['<table>',
+      '  <thead>',
+      '    <tr>',
+      '      <th>Column 1</th>',
+      '      <th>Column 2</th>',
+      '    </tr>',
+      '  </thead>',
+      '  <tbody>',
+      '    <tr>',
+      '      <td>Row 1, Column 1</td>',
+      '      <td>Row 1, Column 2</td>',
+      '    </tr>',
+      '    <tr>',
+      '      <td>Row 2, Column 1</td>',
+      '      <td>Row 2, Column 2</td>',
+      '    </tr>',
+      '  </tbody',
+      '</table>'].join('\n'),
 
-  md = [
-    '| Column 1 | Column 2 |',
-    '| --- | --- |',
-    '| Row 1, Column 1 | Row 1, Column 2 |',
-    '| Row 2, Column 1 | Row 2, Column 2 |'
-  ].join('\n');
+      ['| Column 1 | Column 2 |',
+      '| --- | --- |',
+      '| Row 1, Column 1 | Row 1, Column 2 |',
+      '| Row 2, Column 1 | Row 2, Column 2 |'].join('\n'),
 
-  equal(toMarkdown(html), md, 'Basic table');
+      'Basic table'
+    ],
+    [
+      ['<table>',
+      '  <thead>',
+      '    <tr>',
+      '      <th align="left">Column 1</th>',
+      '      <th align="right">Column 2</th>',
+      '    </tr>',
+      '  </thead>',
+      '  <tbody>',
+      '    <tr>',
+      '      <td>Row 1, Column 1</td>',
+      '      <td>Row 1, Column 2</td>',
+      '    </tr>',
+      '    <tr>',
+      '      <td>Row 2, Column 1</td>',
+      '      <td>Row 2, Column 2</td>',
+      '    </tr>',
+      '  </tbody',
+      '</table>'].join('\n'),
 
-  html = [
-    '<table>',
-    '  <thead>',
-    '    <tr>',
-    '      <th align="left">Column 1</th>',
-    '      <th align="right">Column 2</th>',
-    '    </tr>',
-    '  </thead>',
-    '  <tbody>',
-    '    <tr>',
-    '      <td>Row 1, Column 1</td>',
-    '      <td>Row 1, Column 2</td>',
-    '    </tr>',
-    '    <tr>',
-    '      <td>Row 2, Column 1</td>',
-    '      <td>Row 2, Column 2</td>',
-    '    </tr>',
-    '  </tbody',
-    '</table>'
-  ].join('\n');
+      ['| Column 1 | Column 2 |',
+      '| :-- | --: |',
+      '| Row 1, Column 1 | Row 1, Column 2 |',
+      '| Row 2, Column 1 | Row 2, Column 2 |'].join('\n'),
 
-  md = [
-    '| Column 1 | Column 2 |',
-    '| :-- | --: |',
-    '| Row 1, Column 1 | Row 1, Column 2 |',
-    '| Row 2, Column 1 | Row 2, Column 2 |'
-  ].join('\n');
-
-  equal(toMarkdown(html), md, 'Cell alignment');
+      'Cell alignment'
+    ]
+  ]);
 });
-
-
