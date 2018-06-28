0140cd5a7158674917a0ad1e4d0d171e5a52b387
ayusaf1992 <areeba.yusaf@bbc.co.uk>
2018-06-28 12:38:18 +0100

    rewrote the escape function to escape all markdown characters

--- src/turndown.js
+++ src/turndown.js
@@ -6,6 +6,24 @@ import Node from './node'
 var reduce = Array.prototype.reduce
 var leadingNewLinesRegExp = /^\n*/
 var trailingNewLinesRegExp = /\n*$/
+const markdownReplacements = [
+  [/\\/g, '\\\\'],
+  [/\*/g, '\\*'],
+  [/-/g, '\\-'],
+  [/\+/g, '\\+'],
+  [/=/g, '\\='],
+  [/#/g, '\\#'],
+  [/`/g, '\\`'],
+  [/~/g, '\\~'],
+  [/\|/g, '\\|'],
+  [/\(/g, '\\('],
+  [/\)/g, '\\)'],
+  [/\[/g, '\\['],
+  [/\]/g, '\\]'],
+  [/>/g, '\\>'],
+  [/_/g, '&lowbar;'],
+  [/(\d+)\./g, '$1\\.']
+]
 
 export default function TurndownService (options) {
   if (!(this instanceof TurndownService)) return new TurndownService(options)
@@ -126,47 +144,9 @@ TurndownService.prototype = {
    */
 
   escape: function (string) {
-    return (
+    return markdownReplacements.reduce(
+      (search, replacement) => search.replace(replacement[0], replacement[1]),
       string
-        // Escape backslash escapes!
-        .replace(/\\(\S)/g, '\\\\$1')
-
-        // Escape headings
-        .replace(/^(#{1,6} )/gm, '\\$1')
-
-        // Escape hr
-        .replace(/^([-*_] *){3,}$/gm, function (match, character) {
-          return match.split(character).join('\\' + character)
-        })
-
-        // Escape ol bullet points
-        .replace(/^(\W* {0,3})(\d+)\. /gm, '$1$2\\. ')
-
-        // Escape ul bullet points
-        .replace(/^([^\\\w]*)[*+-] /gm, function (match) {
-          return match.replace(/([*+-])/g, '\\$1')
-        })
-
-        // Escape blockquote indents
-        .replace(/^(\W* {0,3})> /gm, '$1\\> ')
-
-        // Escape em/strong *
-        .replace(/\*+(?![*\s\W]).+?\*+/g, function (match) {
-          return match.replace(/\*/g, '\\*')
-        })
-
-        // Escape em/strong _
-        .replace(/_+(?![_\s\W]).+?_+/g, function (match) {
-          return match.replace(/_/g, '\\_')
-        })
-
-        // Escape code _
-        .replace(/`+(?![`\s\W]).+?`+/g, function (match) {
-          return match.replace(/`/g, '\\`')
-        })
-
-        // Escape link brackets
-        .replace(/[\[\]]/g, '\\$&') // eslint-disable-line no-useless-escape
     )
   }
 }
