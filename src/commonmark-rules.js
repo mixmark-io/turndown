8480f9a0722b92ad994f9dd298da5ed84fb8de09
Dom Christie <christiedom@gmail.com>
2019-01-27 15:12:41 +0000

    Fetch attributes via getAttribute for broader compatibility

--- src/commonmark-rules.js
+++ src/commonmark-rules.js
@@ -109,7 +109,7 @@ rules.fencedCodeBlock = {
   },
 
   replacement: function (content, node, options) {
-    var className = node.firstChild.className || ''
+    var className = node.firstChild.getAttribute('class') || ''
     var language = (className.match(/language-(\S+)/) || [null, ''])[1]
 
     return (
@@ -139,7 +139,7 @@ rules.inlineLink = {
 
   replacement: function (content, node) {
     var href = node.getAttribute('href')
-    var title = node.title ? ' "' + node.title + '"' : ''
+    var title = node.getAttribute('title') ? ' "' + node.getAttribute('title') + '"' : ''
     return '[' + content + '](' + href + title + ')'
   }
 }
@@ -237,9 +237,9 @@ rules.image = {
   filter: 'img',
 
   replacement: function (content, node) {
-    var alt = node.alt || ''
+    var alt = node.getAttribute('alt') || ''
     var src = node.getAttribute('src') || ''
-    var title = node.title || ''
+    var title = node.getAttribute('title') || ''
     var titlePart = title ? ' "' + title + '"' : ''
     return src ? '![' + alt + ']' + '(' + src + titlePart + ')' : ''
   }
