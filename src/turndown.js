5320f09cb252f9da05695e322296816524fac640
Michal Bartoš <michal.bartos@orchitech.cz>
2020-04-15 20:01:22 +0200

    Improve text node replacement

--- src/turndown.js
+++ src/turndown.js
@@ -6,7 +6,7 @@ import Node from './node'
 var reduce = Array.prototype.reduce
 var leadingNewLinesRegExp = /^\n*/
 var trailingNewLinesRegExp = /\n*$/
-var escapes = [
+var ESCAPES = [
   [/\\/g, '\\\\'],
   [/\*/g, '\\*'],
   [/^-/g, '\\-'],
@@ -25,8 +25,10 @@ var escapes = [
 export default function TurndownService (options) {
   if (!(this instanceof TurndownService)) return new TurndownService(options)
 
+  var self = this
   var defaults = {
     rules: COMMONMARK_RULES,
+    escapes: ESCAPES,
     headingStyle: 'setext',
     hr: '* * *',
     bulletListMarker: '*',
@@ -45,6 +47,9 @@ export default function TurndownService (options) {
     },
     defaultReplacement: function (content, node) {
       return node.isBlock ? '\n\n' + content + '\n\n' : content
+    },
+    textReplacement: function (content, node, options) {
+      return node.isCode ? content : self.escape(content, node, options)
     }
   }
   this.options = extend({}, defaults, options)
@@ -140,10 +145,10 @@ TurndownService.prototype = {
    * @type String
    */
 
-  escape: function (string) {
-    return escapes.reduce(function (accumulator, escape) {
+  escape: function (content, node, options) {
+    return options.escapes.reduce(function (accumulator, escape) {
       return accumulator.replace(escape[0], escape[1])
-    }, string)
+    }, content)
   }
 }
 
@@ -162,7 +167,8 @@ function process (parentNode) {
 
     var replacement = ''
     if (node.nodeType === 3) {
-      replacement = node.isCode ? node.nodeValue : self.escape(node.nodeValue)
+      var textRule = self.rules.forNode(node)
+      replacement = textRule.replacement(node.nodeValue, node, self.options)
     } else if (node.nodeType === 1) {
       replacement = replacementForNode.call(self, node)
     }
