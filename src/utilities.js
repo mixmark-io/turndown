ea45dddc5ab55cb3a8e5777a46f55664632b4318
Pavel Horal <pavel.horal@orchitech.cz>
2024-07-16 22:35:36 +0200

    Properly escape inline link and image attributes. Fix #473.

--- src/utilities.js
+++ src/utilities.js
@@ -78,3 +78,25 @@ function has (node, tagNames) {
     })
   )
 }
+
+var markdownEscapes = [
+  [/\\/g, '\\\\'],
+  [/\*/g, '\\*'],
+  [/^-/g, '\\-'],
+  [/^\+ /g, '\\+ '],
+  [/^(=+)/g, '\\$1'],
+  [/^(#{1,6}) /g, '\\$1 '],
+  [/`/g, '\\`'],
+  [/^~~~/g, '\\~~~'],
+  [/\[/g, '\\['],
+  [/\]/g, '\\]'],
+  [/^>/g, '\\>'],
+  [/_/g, '\\_'],
+  [/^(\d+)\. /g, '$1\\. ']
+]
+
+export function escapeMarkdown (string) {
+  return markdownEscapes.reduce(function (accumulator, escape) {
+    return accumulator.replace(escape[0], escape[1])
+  }, string)
+}
